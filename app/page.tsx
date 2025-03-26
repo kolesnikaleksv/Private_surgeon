cae13a4211713c2e0b1a4dcf8760ba0a6c8f7824
kolesnikaleksv <kolesnikaleksv@gmail.com>
2025-03-26 18:13:08 +0200

    Add shadcn library and some component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from '@/components/ui/button';
+import Accordeon from './components/Accordeon';
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl">Hello Main Surgeon</h1>
+      <div className="m-5">
+        <h1 className="text-3xl">Hello Main Surgeon</h1>
+        <Button className="bg-avocado-100">Click me</Button>
+        <Button className="bg-avocado-200">Click me</Button>
+        <hr />
+        <Accordeon />
+      </div>
     </>
   );
 }
